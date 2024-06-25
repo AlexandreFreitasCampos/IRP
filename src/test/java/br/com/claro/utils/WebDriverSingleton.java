0343bffbc01a5268f0b2f7cef45971e21135d374
alexandrefreitascampos <alexandrefreitascampos@gmail.com>
2024-06-25 15:57:35 -0300

    aa

---
+++ src/test/java/br/com/claro/utils/WebDriverSingleton.java
@@ -0,0 +1,26 @@
+package br.com.claro.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverSingleton {
+    private static WebDriver driver;
+
+    private WebDriverSingleton() {}
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().clearDriverCache().setup();
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
